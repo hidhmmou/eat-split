1e2154c7ba44bb68ade91004618930ce8bd5663e
hidhmmou <hassanidhmmou.edu@gmail.com>
2023-12-13 10:03:30 +0100

    static

--- src/static.js
+++ src/static.js
@@ -1,15 +1,63 @@
+// Projects.js
 import React from "react";
+import "./Project.css";
 
-function App() {
+const projects = [
+    {
+        title: "Guess My Number",
+        description: "Guessing a random number; every guess costs one point.",
+        link: "https://hidhmmou.rankupy.com/guessMyNbr/index.html",
+    },
+    {
+        title: "Pig Game",
+        description: "Two-player game; the first one gaining 100pts wins.",
+        link: "https://hidhmmou.rankupy.com/pigGame/index.html",
+    },
+    {
+        title: "Weather App",
+        description:
+            "Weather Application, input your city, hit enter, is it rainy or cloudy?",
+        link: "https://hidhmmou.rankupy.com/weatherApp/index.html",
+    },
+    {
+        title: "To-Do App",
+        description: "To-Do application",
+        link: "https://hidhmmou.rankupy.com/To-Do/index.html",
+    },
+    {
+        title: "Quiz App",
+        description: "Simple Quiz App",
+        link: "https://hidhmmou.rankupy.com/quiz/index.html",
+    },
+    {
+        title: "Travel App",
+        description: "My First React Travel App",
+        link: "",
+    },
+    {
+        title: "Tip Calculator App",
+        description: "React tip calculator application",
+        link: "https://ywn8gk.csb.app/",
+    },
+    // Add more project objects as needed
+];
+
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>My Static React Website</h1>
-                <p>Welcome to my website!</p>
-                <p>My Name is hassan, and this is my static website</p>
+        <React.Fragment>
+            <header>
+                <h1>My Projects :</h1>
             </header>
-        </div>
+            <div className="container">
+                {projects.map((project, index) => (
+                    <a href={project.link} className="project" key={index}>
+                        <h2>{project.title}</h2>
+                        <p>{project.description}</p>
+                    </a>
+                ))}
+            </div>
+        </React.Fragment>
     );
-}
+};
 
 export default App;
