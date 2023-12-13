31d2d4a4692807dead84fcfc26f236b4bf62dccd
hidhmmou <hassanidhmmou.edu@gmail.com>
2023-12-13 09:20:14 +0100

    static

---
+++ src/static.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>My Static React Website</h1>
+                <p>Welcome to my website!</p>
+                <p>My Name is hassan, and this is my static website</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
